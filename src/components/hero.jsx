310cf8a95195a9e5471d122ec9429013d7f686f8
BetaLixT <alp2.qwe@gmail.com>
2021-10-23 17:53:33 +0400

    fixed sizing issues for hero image

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,19 +3,23 @@ export default function Hero (props) {
     // const height = window.innerHeight;
     
     const heroStyle = {
-        display: 'flex',
-        minHeight: props.minHeight
+        minHeight: props.minHeight,
+        overflow: 'hidden',
+        position: 'relative'
     }
     const heroImageStyle = {
         backgroundImage: "url(" + props.backgroundImage + ")",
-        height: "100%",
-        width: "100%"
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundPosition: 'center 15%',
+        backgroundSize: 'cover',
     }
     
     return(
-        <div className="hero" 
-        // ref={(r) => { this.ref = r; }}
-        style={heroStyle}>
+        <div className="hero" style={heroStyle}>
             <div className="hero-image" style={heroImageStyle}/>
             {/* <div className="hero-overlay">
                 {/* {this.props.children && <div>{this.props.children}</div>}}
